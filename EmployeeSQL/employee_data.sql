396715eaf22d827b418b75a3dac5e3418319a84c
LadeeCerumber <demekia.medley@gmail.com>
2023-08-05 22:17:34 -0500

    corrected tables

--- EmployeeSQL/employee_data.sql
+++ EmployeeSQL/employee_data.sql
@@ -24,8 +24,8 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE dept_manager (
-    emp_no INT  NOT NULL,
     dept_no VARCHAR  NOT NULL,
+    emp_no INT  NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (dept_no, emp_no)
